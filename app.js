3917b3f0bfbb3a78cb6e84ab51cca313c957ea1b
Melis Archabaev <melis.archabaev.kg@gmail.com>
2022-12-09 11:56:11 +0600

    Drop and Drop example for learning

---
+++ app.js
@@ -0,0 +1,36 @@
+const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
+
+item.addEventListener("dragstart", dragStart);
+item.addEventListener("dragend", dragEnd);
+
+for(const placeholder of placeholders){
+    placeholder.addEventListener('dragover', dragOver);
+    placeholder.addEventListener('dragenter', dragEnter);
+    placeholder.addEventListener('dragleave', dragLeave);
+    placeholder.addEventListener('drop', dragDrop);
+}
+
+function dragStart(event) {
+  event.target.classList.add("hold");
+  setTimeout(() => {
+    event.target.classList.add("hide");
+  }, 0);
+}
+function dragEnd(e) {
+  e.target.classList.remove("hold", "hide");
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
+function dragEnter(e){
+    e.target.classList.add("hovered");
+}
+function dragLeave(e){
+    e.target.classList.remove("hovered");
+}
+function dragDrop(e){
+    e.target.append(item);
+    e.target.classList.remove("hovered");
+}
